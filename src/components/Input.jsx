82d9b6fdaebdb83744c0c62e7bec5ac6b72e7814
inhachoi <80045891+inhachoi@users.noreply.github.com>
2024-03-20 19:50:13 +0900

    [FEAT] 로그인 회원가입 모달 추가

--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -1,45 +1,19 @@
 import styled from 'styled-components';
 import palette from '../styles/palette';
 
-function Input({ id, placeholder, type, value, label, onChange, ...styles }) {
-  return (
-    <InputBox>
-      {label && (
-        <label htmlFor={id}>
-          <p>{label}</p>
-        </label>
-      )}
-      <input placeholder={placeholder} type={type} value={value} id={id} onChange={onChange} {...styles} />
-    </InputBox>
-  );
-}
-export default Input;
+const Input = styled.input`
+  box-sizing: border-box;
+  width: 100%;
+  padding: 12px 16px;
+  border-radius: 16px;
+  border: 2px solid #cdcdcd;
+  color: ${palette.black[0]};
+  font-size: 16px;
 
-const InputBox = styled.div`
-  display: flex;
-  flex-direction: column;
-  gap: 0px;
-
-  p {
-    font-size: 12px;
-    margin: 0;
-    padding-bottom: 8px;
-    line-height: 1;
-    cursor: pointer;
-  }
-
-  input {
-    box-sizing: border-box;
-    width: 100%;
-    padding: 12px 16px;
-    border-radius: 16px;
-    border: 2px solid #cdcdcd;
-    color: ${palette.black[0]};
-    font-size: 16px;
-
-    &:focus {
-      box-shadow: 0 0 0 4px rgba(0, 132, 255, 0.5);
-      outline: 0;
-    }
+  &:focus {
+    box-shadow: 0 0 0 4px rgba(0, 132, 255, 0.5);
+    outline: 0;
   }
 `;
+
+export default Input;
