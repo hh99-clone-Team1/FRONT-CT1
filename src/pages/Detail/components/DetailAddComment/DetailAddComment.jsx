7be7d22e76182826af52c7ad01084a66eee2e85f
vjiji <restmi0711@gmail.com>
2024-03-22 03:03:14 +0900

    feat: detail 페이지의 댓글 작성 컴포넌트 레이아웃 작업

--- src/pages/Detail/components/DetailAddComment/DetailAddComment.jsx
+++ src/pages/Detail/components/DetailAddComment/DetailAddComment.jsx
@@ -1,12 +1,40 @@
+/* eslint-disable react/style-prop-object */
 import styled from 'styled-components';
+import { Link } from 'react-router-dom';
+import { useState } from 'react';
 import palette from '../../../../styles/palette';
+import likeIcon from '../../../../img/likeIcon.svg';
+import likeSelectedIcon from '../../../../img/likeSelectedIcon.svg';
+import AddCommentIcon from '../../../../img/AddCommentIcon';
 
-function DetailAddComment() {
+function DetailAddComment({ commentsCount }) {
+  const isLike = '좋아요';
+  const user = 'test';
+  const [comment, setComment] = useState('');
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(comment);
+  };
   return (
     <DetailAddCommentLayout>
       <CommentBox>
-        <div>댓글, 좋아요</div>
-        <div>인풋</div>
+        <CountBox $isLike={isLike}>
+          <h3>댓글 {commentsCount}개</h3>
+          <div>
+            <img src={isLike ? likeSelectedIcon : likeIcon} alt="like" />
+          </div>
+        </CountBox>
+        <InputBox onSubmit={handleSubmit}>
+          <Profile role="presentation" to={() => '유저페이지로 이동'}>
+            {user.split('')[0]}
+          </Profile>
+          <label htmlFor="comment-input">
+            <input id="comment-input" autoComplete="none" onChange={(e) => setComment(e.currentTarget.value)} />
+            <ButtonBox type="submit" $active={comment}>
+              <AddCommentIcon />
+            </ButtonBox>
+          </label>
+        </InputBox>
       </CommentBox>
     </DetailAddCommentLayout>
   );
@@ -27,6 +55,89 @@ const CommentBox = styled.div`
   border-top: 1px solid ${palette.gray[1]};
   display: flex;
   flex-direction: column;
-
   background: #fff;
+`;
+
+const CountBox = styled.div`
+  height: 44px;
+  margin: 4px 0 12px;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+
+  h3 {
+    font-size: 20px;
+    font-weight: 600;
+  }
+
+  div {
+    min-width: 48px;
+    min-height: 48px;
+    border-radius: 24px;
+    background: ${({ $isLike }) => ($isLike ? palette.red[4] : palette.gray[2])};
+    display: flex;
+    align-items: center;
+    justify-content: center;
+  }
+`;
+
+const InputBox = styled.form`
+  margin-bottom: 12px;
+  display: flex;
+  align-items: flex-end;
+
+  gap: 8px;
+
+  label {
+    border-radius: 24px;
+    min-height: 52px;
+    width: 100%;
+    border: 2px solid ${palette.gray[2]};
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    padding: 0 6px 0 16px;
+  }
+
+  input {
+    width: 260px;
+    border: none;
+    font-size: 16px;
+    outline: none;
+  }
+`;
+
+const Profile = styled(Link)`
+  text-decoration: none;
+  min-width: 48px;
+  height: 48px;
+  margin-top: 16px;
+  border-radius: 25px;
+  background: skyblue;
+  color: #fff;
+  font-size: 20px;
+  font-weight: 600;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
+
+const ButtonBox = styled.button`
+  width: 40px;
+  height: 40px;
+  border: none;
+  border-radius: 25px;
+  display: ${({ $active }) => ($active ? 'flex' : 'none')};
+  align-items: center;
+  justify-content: center;
+  background-color: ${palette.red[3]};
+  cursor: pointer;
+
+  &:hover {
+    background-color: ${palette.red[0]};
+  }
+
+  svg {
+    fill: #fff;
+  }
 `;
--- src/pages/Detail/components/DetailContents/DetailContents.jsx
+++ src/pages/Detail/components/DetailContents/DetailContents.jsx
@@ -13,9 +13,11 @@ const getPostDetail = async (id) => {
     },
   });
   // Todo : image 정보 들어오면 return 변경
+
   return {
     ...data,
     image: 'https://i.pinimg.com/564x/63/2e/6b/632e6b7d7e460e2b19f0a679dda0f500.jpg',
+    nickname: 'test',
   };
 };
 
