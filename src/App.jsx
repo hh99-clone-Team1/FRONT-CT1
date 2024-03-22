d25c69143c84217126ac53880807155f98fb273d
cherryl-dev <chaeliim75@gmail.com>
2024-03-22 09:56:35 +0900

    [CONFLICT] App.jsx 충돌 문제 해결

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,8 @@ import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import './App.css';
 import Router from './shared/router';
 
+const queryClient = new QueryClient();
+
 function App() {
   return (
     <QueryClientProvider client={queryClient}>
