12162c32722a3b3ef99ecf32780519c8d434fca7
inhachoi <80045891+inhachoi@users.noreply.github.com>
2024-03-23 01:14:52 +0900

    [STYLE] Intro페이지 분리

---
+++ src/pages/Intro/Intro.jsx
@@ -0,0 +1,50 @@
+/* eslint-disable*/
+import { useState } from 'react';
+import Button from '../../components/Button';
+import LoginSignupModal from './LoginSignupModal';
+import { IntroHeaderLayout, HeaderLogoBox, HearderSearchBox, HeaderChoiceBox } from './Intro.module';
+
+function Intro() {
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const [isLogin, setIsLogin] = useState(true);
+  const openModal = () => {
+    setIsModalOpen(true);
+  };
+  const closeModal = () => {
+    setIsModalOpen(false);
+  };
+
+  const handleLogin = () => {
+    setIsLogin(true);
+    openModal();
+  };
+  const handleSignup = () => {
+    setIsLogin(false);
+    openModal();
+  };
+
+  return (
+    <>
+      <IntroHeaderLayout>
+        <div>
+          <HeaderLogoBox>Pinterest </HeaderLogoBox>
+          <HearderSearchBox>탐색 </HearderSearchBox>
+        </div>
+        <div>
+          <HeaderChoiceBox>소개 </HeaderChoiceBox>
+          <HeaderChoiceBox>비즈니스 </HeaderChoiceBox>
+          <HeaderChoiceBox>언론 </HeaderChoiceBox>
+          <Button LightRed type="button" onClick={handleLogin}>
+            로그인
+          </Button>
+          <Button LightGray type="button" onClick={handleSignup}>
+            가입하기
+          </Button>
+        </div>
+      </IntroHeaderLayout>
+      {isModalOpen && <LoginSignupModal closeModal={closeModal} isLogin={isLogin} setIsLogin={setIsLogin} />}
+    </>
+  );
+}
+
+export default Intro;
