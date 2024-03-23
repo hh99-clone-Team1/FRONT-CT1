355c10cd721a51402eeae579589b60f4211a8819
inhachoi <80045891+inhachoi@users.noreply.github.com>
2024-03-23 18:47:44 +0900

    [STYLE] 인트로 페이지 디자인

--- src/pages/Intro/InfoArea.jsx
+++ src/pages/Intro/InfoArea.jsx
@@ -1,18 +1,24 @@
-import React from 'react';
+import { useState, useEffect } from 'react';
 import PartFirst from './PartFirst';
 import PartSecond from './PartSecond';
 import PartThird from './PartThird';
-import PartFourth from './PartFourth';
-import PartFifth from './PartFifth';
 
 function InfoArea() {
+  const [rerenderCount, setRerenderCount] = useState(0);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setRerenderCount((prevCount) => prevCount + 1);
+    }, 5000);
+
+    return () => clearInterval(interval);
+  }, []);
+
   return (
     <>
-      <PartFirst />
+      <PartFirst key={rerenderCount} />
       <PartSecond />
       <PartThird />
-      <PartFourth />
-      <PartFifth />
     </>
   );
 }
